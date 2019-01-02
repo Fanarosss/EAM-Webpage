99a186aa53cb1f97902dcc039234f35178b62883
TheoProgramming <bikosenter@gmail.com>
2019-01-02 18:06:56 +0200

    navbar

--- index.php
+++ index.php
@@ -5,10 +5,52 @@
     <meta name="viewport" content="width=device-width">
     <link rel="icon" type="image/png" href="images/icon.png">
     <title>Eudoxus</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 </head>
-<body>
-  <a href="index.php"><img src="images/eudoxus.png"/></a>
 
+<style>
+  div.logo: {
+    float: left;
+  }
+
+  div.mynav: {
+    margin-left: 100px;
+  }
+</style>
+
+<body>
+  <div class="logo">
+    <a href="index.php"><img src="images/eudoxus.png"/></a>
+  </div>
+  <!-- Navigation -->
+  <div class="mynav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </div>
   <?php
     echo "Project 3";
     ?>
