1088ca1a4432671ee68519e4e20a14df63fdf265
TheoProgramming <bikosenter@gmail.com>
2019-01-02 16:48:45 +0200

    favicon

--- src/index.php
+++ src/index.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="icon" type="image/png" href="icon.png">
+    <title>Eudoxus</title>
+</head>
 <body>
 
   <?php
