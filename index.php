7a612f6bc69ff88b19465845dc528753edcb2eaf
TheoProgramming <bikosenter@gmail.com>
2019-01-02 17:21:25 +0200

    Reorganize

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="icon" type="image/png" href="icon.png">
+    <link rel="icon" type="image/png" href="images/icon.png">
     <title>Eudoxus</title>
 </head>
 <body>
+  <a href="index.php"><img src="images/eudoxus.png"/></a>
 
   <?php
     echo "Project 3";
