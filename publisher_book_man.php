930198333d290a9ce255b0bb2034d27e7d7148a0
Kostas <kostasA97@gmail.com>
2019-01-09 00:37:54 +0200

    Book Management

--- publisher_book_man.php
+++ publisher_book_man.php
@@ -74,6 +74,60 @@
         <a class="nav-link" href="https://eudoxus.gr/files/ManualPublishersUpdateBooks.pdf" target="_blank">Manual</a>
       </li>
     </ul>
+    <div class="bs-item3">
+      <div class="jumbotron2">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <a class="btn btn-primary btn-lg" href="http://localhost/publisher_settings.php" role="button"><i class="fas fa-book"></i>  Add Book</a>
+            <form action="publisher_book_man.php" method="POST">
+              <div class="form-inline my-2 my-lg-0" style="margin-left: 20px">
+                <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit" name="submit-search">Search</button>
+                <div class="form-group">
+                  <select class="btn btn-primary btn-lg" style="margin-left: 20px">
+                    <option disabled selected value>-- Sort By --</option>
+                    <option>Id</option>
+                    <option>Title</option>
+                    <option>Author</option>
+                    <option>ISBN</option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
+        </nav>
+        <table class="table table-hover" style="margin-top: 20px">
+          <thead>
+            <tr>
+              <th scope="col">Available</th>
+              <th scope="col">ID</th>
+              <th scope="col">Title</th>
+              <th scope="col">Author</th>
+              <th scope="col">ISBN</th>
+              <th scope="col">Registration</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            include './src/config.php';
+            $p = $_SESSION['Username'];
+            if (isset($_POST['submit-search'])){
+              $search = mysqli_real_escape_string($conn, $_POST['search']);
+              $query = "SELECT * FROM book WHERE Publications = '$p' AND (Id LIKE '%$search%' OR Title LIKE '%$search%' OR Author LIKE '%$search%'
+                        OR ISBN LIKE '%$search%')";
+              $result = $conn->query($query);
+            } else {
+              $query = "SELECT * FROM book WHERE Publications = '$p'";
+              $result = $conn->query($query);
+            }
+            while($row = $result->fetch_assoc()){   //Creates a loop to loop through results
+              echo "<tr><td>x</td><td>" . $row['Id'] . "</td><td>" . $row['Title'] . "</td><td>" . $row['Author'] . "</td><td>" . $row['ISBN'] . "</td><td>x</td></tr>";
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </body>
 </html>
